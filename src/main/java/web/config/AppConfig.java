64d8da23fa3f0d2995e7f8c3d40af38e5494d3d4
A.Sh <qqquylqu@gmail.com>
2024-01-12 22:50:20 +0300

    first commit

---
+++ src/main/java/web/config/AppConfig.java
@@ -0,0 +1,68 @@
+package web.config;
+
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+
+@Configuration
+@PropertySource("classpath:db.properties")
+@EnableTransactionManagement
+@ComponentScan(value = "web.config")
+public class AppConfig {
+
+    @Resource
+    private Environment env;
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan(env.getRequiredProperty("db.entity.package"));
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        em.setJpaProperties(additionalProperties());
+        return em;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return transactionManager;
+    }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+
+    Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        return properties;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setUrl(env.getRequiredProperty("db.url"));
+        ds.setDriverClassName(env.getRequiredProperty("db.driver"));
+        ds.setUsername(env.getProperty("db.username"));
+        ds.setPassword(env.getProperty("db.password"));
+        return ds;
+    }
+}
---
+++ src/main/java/web/controller/UsersController.java
@@ -0,0 +1,77 @@
+package web.controller;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import web.dao.UserDao;
+import web.model.User;
+
+@Controller
+@RequestMapping("/users")
+public class UsersController {
+
+    private final UserDao userDAO;
+
+    @Autowired
+    public UsersController(UserDao userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    @GetMapping()
+    public String getAllUsers(Model model) {
+        model.addAttribute("users", userDAO.getAllUsers());
+        return "users";
+    }
+
+    @GetMapping("/new")
+    public String newUser(Model model) {
+        model.addAttribute("user", new User());
+        return "new";
+    }
+
+    @PostMapping()
+    @Transactional
+    public String create(@ModelAttribute("user") User user) {
+        userDAO.save(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/idForUpdate")
+    public String showUpdateUserForm() {
+        return "idForUpdate";
+    }
+
+    @PostMapping("/idForUpdate")
+    public String processUpdateUserForm(@RequestParam("userId") Long userId) {
+        return "redirect:/users/update/" + userId;
+    }
+
+    @GetMapping("/update/{id}")
+    public String updateUserByID(@PathVariable("id") Long id, Model model) {
+        User user = userDAO.getUserById(id);
+        model.addAttribute("user", user);
+        return "update";
+    }
+
+    @PostMapping("/update/{id}")
+    @Transactional
+    public String update(@RequestParam("id") Long id, @ModelAttribute("user") User user) {
+        user.setId(id);
+        userDAO.update(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/idForDelete")
+    public String showDeleteUserForm() {
+        return "idForDelete";
+    }
+
+    @PostMapping("/idForDelete")
+    @Transactional
+    public String processDeleteUserForm(@RequestParam("userId") Long userId) {
+        userDAO.delete(userId);
+        return "redirect:/users";
+    }
+}
